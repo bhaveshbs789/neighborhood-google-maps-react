2b408932d13ea15df180706caa0d1e02dee21e3d
Bhavesh Sharma <bhaveshbs789@gmail.com>
2018-12-04 20:49:01 +0530

    Added FourSquare API, and centered the results

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Map from './Map'
+import * as FourSquareAPI from './FourSquareAPI'
 
 class App extends Component {
+
+  state = {
+    allLocations : [],
+    markers : [],
+    center : [],
+    zoom : 13
+
+  }
+
+  componentDidMount(){
+    FourSquareAPI.searchVenue({
+        near: "Hyderabad, IN",
+        query: "biryani",
+        limit: 10
+      }).then(results => {
+        console.log(results)
+        const { venues } = results.response;
+        const { center } = results.response.geocode.feature.geometry;
+
+        const markers = venues.map(venue => {
+          return {
+            lat: venue.location.lat,
+            lng: venue.location.lng
+          }
+        })
+
+        this.setState({allLocations: venues, markers: markers, center: center})
+      })
+
+
+  }
+  
   render() {
     return (
       <div className="App">
         <div>
           <h1>Biryani Locations, Hyderabad</h1>
         </div>
-        <Map />
+        <Map {...this.state}/>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+// import * as FourSquareAPI from './FourSquareAPI';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 const api_key = "AIzaSyCL1A0cRaE7FO0bNKY3U2rJSfws0Z9z4-Q";
 
-export class MapContainer extends React.Component {
+export class MapContainer extends Component {
+	
+	state = {
+			markers : [],
+			map : null,
+			showingInfoWindow: false,
+			locations: []
+	}
+
+	mapReady = (mapProps, map) => {
+		this.setState({map: map})
+	}
+
+	componentDidMount() {
+		
+	}
+
 	render() {
+		console.log(this.props)
+
+		const style = {
+			width: "100%",
+			height: "100%"
+		}
+
+		const center = {
+			lat: this.props.center.lat,
+			lng: this.props.center.lng
+		}
+
+		// console.log(`Value for center, ${center}`);
 		return(
-			<Map google={this.props.google} zoom={14}>
+			<Map
+				role="application"
+				aria-label="map"
+				google={this.props.google}
+				center={center}
+				zoom={this.props.zoom}
+				style={style}
+				onReady={this.mapReady}>
         	</Map>
 		)
 		
